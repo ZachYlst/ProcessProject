5bc72f3eafbf150ccb6bcd5d9e7fff70abc88626
ZachYlst <ZachYlst@users.noreply.github.com>
2017-09-07 08:14:20 -0600

    Started the runner file.

---
+++ src/sample/controller/SampleController.java
@@ -0,0 +1,11 @@
+package sample.controller;
+
+public class SampleController 
+{
+
+	public void start()
+	{
+		System.out.println("This is a sample project to show the Github and");
+		System.out.println("Eclipse process so I develop muscle memory");
+	}
+}
