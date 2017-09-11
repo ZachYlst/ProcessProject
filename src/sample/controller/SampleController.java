752f85da8032353aa51927eec91a1ff6881e8aad
ZachYlst <ZachYlst@users.noreply.github.com>
2017-09-11 08:44:34 -0600

    Added another PlayDohClass.

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,11 +1,27 @@
 package sample.controller;
 
 import java.util.Scanner;
+import sample.model.PlayDohCircle;
+import sample.model.PlayDohSquare;
 
 public class SampleController
 {
+	public SampleController()
+	{
+		
+	}
+	
 	public void start()
 	{
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle();
+		
+		PlayDohCircle thirdCircle;
+		thirdCircle = new PlayDohCircle(10);
+		
+		PlayDohSquare mySquare = new PlaydohSquare(5);
+		
 		System.out.println("This is a sample project to show the Github and");
 		System.out.println("Eclipse process so I develop muscle memory");
 		
@@ -15,7 +31,7 @@ public class SampleController
 private void testScanner()
 	{
 		Scanner firstScanner; //Declares a Scanner object 
-		firstScanner = new Scanner(System.in);  //Instanciates a Scanner object
+		firstScanner = new Scanner(System.in);  //Instantiates a Scanner object
 		System.out.println("Please type your favorite food");
 		String answer = firstScanner.nextLine();
 		System.out.println("Oh, you like to eat " + answer);
---
+++ src/sample/model/PlayDohSquare.java
@@ -0,0 +1,16 @@
+package sample.model;
+
+public class PlayDohSquare 
+{
+	private int length;
+	
+	public PlayDohSquare()
+	{
+		this.length = 15;
+	}
+	
+	public PlayDohSquare(int length)
+	{
+		this.length = length;
+	}
+}
